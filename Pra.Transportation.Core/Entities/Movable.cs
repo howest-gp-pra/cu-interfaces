c2f5c7a3507586a5f516dd8f06137c2a1fcc6f51
StefVercaemer <stef.vercaemer@spermalie.be>
2021-06-22 18:34:24 +0200

    Add Movable and IMovable to class lib

---
+++ Pra.Transportation.Core/Entities/Movable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Pra.Transportation.Core.Entities
+{
+    public abstract class Movable
+    {
+        protected static Random random = new Random();
+
+        public TimeSpan TimePerKilometer
+        {
+            get
+            {
+                TimeSpan hour = TimeSpan.FromHours(1);
+                return hour / AverageSpeed;
+            }
+        }
+
+        public virtual string TransportationInfo
+        {
+            get { return $"{Model}\n\t{AverageSpeed} km/u."; }
+        }
+
+        public float AverageSpeed { get; set; }
+
+        public string Model { get; set; }
+
+        public Movable(float averageSpeed, string model)
+        {
+            AverageSpeed = averageSpeed;
+            Model = model;
+        }
+        public abstract TimeSpan Move(float kilometers);
+
+        public override string ToString()
+        {
+            return TransportationInfo;
+        }
+    }
+}
---
+++ Pra.Transportation.Core/Interfaces/IMovable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pra.Transportation.Core.Interfaces
+{
+    public interface IMovable
+    {
+        float AverageSpeed { get;  }
+        string TransportationInfo { get; }
+        TimeSpan Move(float kilometers);
+    }
+}
