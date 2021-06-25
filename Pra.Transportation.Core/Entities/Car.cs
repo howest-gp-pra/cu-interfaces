8715dc2f79e5c138a425af927bd22dab57307eba
StefVercaemer <stef.vercaemer@spermalie.be>
2021-06-25 21:57:01 +0200

    Create Car

---
+++ Pra.Transportation.Core/Entities/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pra.Transportation.Core.Entities
+{
+    public class Car : Movable
+    {
+
+        public Car(float averageSpeed, string type, float averageConsumption) : base(averageSpeed, type)
+        {
+            
+        }
+
+        public override TimeSpan Move(float kilometers)
+        {
+            // Efectieve implementatie volgt verder
+            return new TimeSpan(0);
+        }
+    }
+}
