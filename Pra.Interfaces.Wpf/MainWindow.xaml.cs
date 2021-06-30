7eb6da99c3cb1b5349e047b8148dfa9e05b886a2
Herman De Beukelaer <herman.de.beukelaer@howest.be>
2021-06-30 15:16:15 +0200

    Clean up

--- Pra.Interfaces.Wpf/MainWindow.xaml.cs
+++ Pra.Interfaces.Wpf/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Pra.Interfaces.Wpf
 {
@@ -23,13 +12,6 @@ namespace Pra.Interfaces.Wpf
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        void ShowFeedback(string feedback, bool isError = true)
-        {
-            tbkFeedBack.Visibility = Visibility.Visible;
-            tbkFeedBack.Text = feedback;
-            tbkFeedBack.Background = isError ? Brushes.IndianRed : Brushes.DeepSkyBlue;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
